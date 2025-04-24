1135b5e4805dce9ca5028fe7125a2766876d1a0f
Vivian Pang <vpang@usc.edu>
2025-04-24 00:59:15 +0000

    Push files

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,17 +21,29 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+bool schedHelper(
+  const AvailabilityMatrix& avail,
+  const size_t dailyNeed,
+  const size_t maxShifts,
+  vector<size_t> assignedShifts,
+  DailySchedule& schedule,
+  int currDay, // the current day
+  int currSlot //number of slots in a day
+);
 
 
 // Add your implementation of schedule() and other helper functions here
 
+
+//d =max maxShifts
+//k = possible workers [numWorkers]
+//d = needed workers
 bool schedule(
     const AvailabilityMatrix& avail,
     const size_t dailyNeed,
     const size_t maxShifts,
     DailySchedule& sched
-)
-{
+){
     if(avail.size() == 0U){
         return false;
     }
@@ -39,7 +51,75 @@ bool schedule(
     // Add your code below
 
 
+    int numDays = avail.size();
+    int numWorkers = avail[0].size();
+    sched.resize(numDays, vector<Worker_T> (dailyNeed, INVALID_ID));
 
+    vector<size_t> assignedShifts(numWorkers,0);
+
+    return schedHelper(avail, dailyNeed, maxShifts, assignedShifts, sched,0,0);
 
 }
 
+
+bool schedHelper(
+  const AvailabilityMatrix& avail,
+  const size_t dailyNeed,
+  const size_t maxShifts,
+  vector<size_t> assignedShifts,
+  DailySchedule& schedule,
+  int currDay, // the current day
+  int currSlot //number of slots in a day
+  ){
+
+    int numDays = avail.size();
+    int numWorkers= avail[0].size();
+
+    if(currDay==numDays){
+      return true;
+      
+    }
+
+    if(currSlot == dailyNeed){ //filled all slots for the day, move to next
+      return schedHelper(avail, dailyNeed, maxShifts, assignedShifts, schedule, currDay+1, 0);
+    }
+
+    //try to assign each worker to the current slot, need to check if they are also available
+    for(Worker_T i =0; i<numWorkers;i++){
+      if(assignedShifts[i]<maxShifts&&avail[currDay][i]){
+        bool scheduled = false;
+        for(int j = 0; j<currSlot;j++){
+          if(schedule[currDay][j]==i){
+            scheduled = true;
+            break;
+          }
+        }
+
+
+        //worker aviable/not scheduled, assign to the shift
+        if(scheduled == false){
+          assignedShifts[i]++;
+          schedule[currDay][currSlot] = i;
+
+
+          if(schedHelper(avail, dailyNeed, maxShifts, assignedShifts, schedule, currDay, currSlot+1)){
+            return true;
+          }
+
+
+
+          schedule[currDay][currSlot] = INVALID_ID;
+          assignedShifts[i]-=1;
+
+
+
+        }
+
+
+      }
+    }
+
+    //cannot assign
+    return false;
+
+}
--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,8 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-void findWords();
+void findWords(string& in, string floating, const set<string>& dict, 
+  int pos, set<string>& resultWords);
 
 
 // Definition of primary wordle function
@@ -23,8 +24,45 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
-    findWords(string& in, string& floating, set<string>& dict,
-     string& currBuiltWord, int pos, set<string>& resultWords);
+    // set<string> filteredDict; //for words with correct length
+    // for(set<string>::iterator it = dict.begin();it!= dict.end();++it){
+    //   bool matchesLength = true;
+    //   for(int i =0;i<in.length();i++){
+    //     if(in[i] != '-' && in[i] !=((*it)[i])){
+    //       matchesLength = false;
+    //       break;
+
+    //     }
+
+    //     if(matchesLength){
+    //       filteredDict.insert(*it);
+
+    //     }
+
+    //   }
+
+    // }
+
+    
+    set<string> resultWords;
+    string currBuiltWord = in;
+    int dashes =0;
+    for(char i:currBuiltWord){
+      if(i =='-'){
+        dashes++;
+
+      }
+
+    }
+
+    if(dashes<floating.size()){
+      return resultWords;
+
+    }
+
+    findWords(currBuiltWord, floating,dict,0,resultWords);
+
+    return resultWords;
 
 }
 
@@ -36,36 +74,138 @@ std::set<std::string> wordle(
 // fixed is green letters
 // -i-: 3 letter word with i in the middle (stirng& in)
 void findWords(
-  string& in, string& floating, set<string>& dict, 
-  string& currBuiltWord, int pos, set<string>& resultWords){
+  string& in, string floating, const set<string>& dict, 
+  int pos, set<string>& resultWords){
 
-  //base case
-  if(pos == in.length()){
-    if(dict.find(currBuiltWord) != dict.end()){
-      resultWords.insert(currBuiltWord);
+  set<char> usedLetters;
+
+
+  if(pos == in.size()){
+    if(dict.find(in) != dict.end() && floating.empty()){
+      resultWords.insert(in);
     }
     return;
   }
 
-  if(pos[in]!='-'){ //on a green letter
-    findWords(in, floating, dict, currBuiltWord, pos+1, resultWords);
+
+  if(in[pos]!= '-'){ //on a green letter
+    findWords(in, floating, dict, pos+1, resultWords);
     return;
   }
 
-  //try yellow letters at the pos
-  for(int i =0; i<floating.length();i++){
-    currBuiltWord[pos] = floating[i];
-    string newFloating=floating;
-    newFloating.erase(i,1);//removes the letter at this pos in floating string
-
-    findWords(in, newFloating, dict, currBuiltWord, pos+1, resultWords);
-  }
-
-  for(char i = 'a';i<='z';i++){
-    if(floating.find(i)==string::npos){
-      currBuiltWord[pos] = i;
-      return findWords(in, floating, dict, currBuiltWord, pos+1, resultWords);
+  int remainingSlots = 0;
+  for(size_t i = pos; i <in.size();i++){
+    if(in[i]=='-'){
+      remainingSlots++;
     }
+
   }
+
+  for(size_t i=0;i<floating.size();i++){
+    if(usedLetters.find(floating[i]) != usedLetters.end()){
+      continue;
+
+    }
+
+    in[pos] = floating[i];
+    usedLetters.insert(floating[i]);
+
+    
+    string newFloating = floating;
+    newFloating.erase(i,1);
+
+    findWords(in, newFloating, dict, pos+1, resultWords);
+
+    in[pos] = '-';
+
+  }
+
+  if(!(remainingSlots<=floating.size())){
+    for(char c = 'a';c<='z';c++){
+      if(usedLetters.find(floating[c]) != usedLetters.end()){
+        continue;
+      }
+      in[pos]=c;
+      findWords(in, floating, dict, pos+1, resultWords);
+      in[pos]='-';
+
+    }
+
+  }
+
+  // //try yellow letters at the pos
+  // for(int i =0; i<floating.length();i++){
+  //   currBuiltWord[pos] = floating[i];
+  //   string newFloating=floating.substr(0,i)+floating.substr(i+1); //removes the letter at this pos in floating string
+
+  //   bool valid = false;
+  //   for(set<string>::iterator it = dict.begin(); it!= dict.end();++it){
+  //     if(it->substr(0,pos+1)==currBuiltWord.substr(0,pos+1)){
+  //       valid = true;
+  //       break;
+
+  //     }
+
+  //   }
+
+  //   if(valid){
+  //     findWords(in, newFloating, dict, currBuiltWord, pos+1, resultWords);
+  //   }
+  // }
+
+  // int remainingSlots = 0;
+  // for(int i = 0;i <in.length();i++){
+  //   if(in[i]=='-'){
+  //     remainingSlots++;
+
+  //   }
+
+  // }
+
+  // if(remainingSlots>(int)floating.length()){
+
+
+  //   // for(int i =0; i<floating.length();i++){
+  //   //   currBuiltWord[pos] = floating[i];
+  //   //   string newFloating=floating;
+  //   //   newFloating.erase(i,1);//removes the letter at this pos in floating string
+
+  //   //   findWords(in, newFloating, dict, currBuiltWord, pos+1, resultWords);
+  //   // }
+
+
+
+  //     for(char i = 'a';i<='z';i++){
+  //       bool inYellow = false;
+  //       for(int j = 0; j<floating.length();j++){
+  //         if(floating[j]==i){
+  //           inYellow=true;
+  //           break;
+
+  //         }
+
+  //       }
+  //       if(inYellow==false){
+  //         currBuiltWord[pos] = i;
+
+  //         bool valid = false;
+  //         for(set<string>::iterator it = dict.begin(); it!= dict.end();++it){
+  //           if(it->substr(0,pos+1)==currBuiltWord.substr(0,pos+1)){
+  //             valid = true;
+  //             break;
+
+  //           }
+
+  //         }
+
+  //         if(valid){
+  //           findWords(in, newFloating, dict, currBuiltWord, pos+1, resultWords);
+  //         }
+  //         // findWords(in, floating, dict, currBuiltWord, pos+1, resultWords);
+  //       }
+  //     }
+  //     return;
+
+  // }
 
 }
